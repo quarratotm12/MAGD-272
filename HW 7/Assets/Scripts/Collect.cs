5ae78be69a5e9b35a3091253131f53d0ffe66abe
quarratotm12 <Tyler@Tylers-MacBook-Pro-6.local>
2016-04-04 00:09:33 -0500

    HW 7
    HW showing robot and lion, and the difference in movement

---
+++ HW 7/Assets/Scripts/Collect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collect : MonoBehaviour {
+
+	public AudioSource fart;
+
+	void OnTriggerEnter2D(Collider2D colli){  //Update () {
+		if(colli.CompareTag("Collectable")){
+			colli.gameObject.SetActive (false);
+			fart.Play ();
+			Debug.Log ("touched");
+		}
+	}
+}
---
+++ HW 7/Assets/Scripts/character.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character : MonoBehaviour {
+
+	Rigidbody2D charRB;
+
+	float hVelocity; // store the horizontal velocity
+	float vVelocity; // store the vertical velocity
+
+	public float speedOffset = 0.2f;  // speed offset for horizontal movement, otherwise character zooms off screen
+
+	public float jumpHeight = 7f; // the jump velocity.
+
+	public float secondJumpHeight = 7.5f; // the second jump velocity.
+	public int numOfJumps; //check for total number of jumps
+
+	public bool onGround; // check for when character is on the ground
+
+
+	// Use this for initialization
+	void Start () {
+		charRB = gameObject.GetComponent<Rigidbody2D> ();
+		numOfJumps = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hVelocity = Input.GetAxis ("Horizontal") * speedOffset;
+		getJump ();
+		move ();
+	}
+
+
+	void move(){
+
+		charRB.transform.position = new Vector2 (charRB.transform.position.x + hVelocity, charRB.transform.position.y);
+		charRB.velocity += (Vector2.up * vVelocity);
+	}
+
+	void getJump(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if(numOfJumps == 1){ 
+				charRB.velocity = Vector2.zero;
+				vVelocity = secondJumpHeight;
+				numOfJumps++;
+			}
+			if (onGround) {
+				vVelocity = jumpHeight;
+				numOfJumps++;
+			}
+		} else {
+			vVelocity = 0;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll){ //check for touching ground
+		if(coll.CompareTag("Ground")){
+			onGround = true;
+			numOfJumps = 0;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D coll){ //check for leaving ground
+		if (coll.CompareTag ("Ground")) {
+			onGround = false;
+		}
+	}
+		
+
+}
